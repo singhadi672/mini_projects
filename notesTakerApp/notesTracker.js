c039322a531213fd4bc260b37da46abfff6355f4
singhadi672 <70839024+singhadi672@users.noreply.github.com>
2020-11-10 11:00:55 +0530

    Add files via upload
    added modal functionality and fixed issues

--- notesTakerApp/notesTracker.js
+++ notesTakerApp/notesTracker.js
@@ -1,14 +1,11 @@
 var i=0;
 
-
 function func1(){
-    
     var desc = document.getElementById("notes").value;
     if(!desc){
         alert("Please enter a note!");
     }
     else{
-
         var nodeMain = document.createElement("div");
         var header = document.createElement("h1");
         header.innerHTML = "Note " + (++i);
@@ -17,7 +14,6 @@ function func1(){
         var button = document.createElement("button");
         button.innerHTML = "View Full Note!";
         button.id = "viewFull";
-        
         
         paragraph.innerHTML = desc;
         nodeMain.appendChild(header);
@@ -28,28 +24,22 @@ function func1(){
         document.getElementById("container").appendChild(nodeMain);
 
         document.getElementById("notes").value = "";
-
         var modal = document.getElementById("myModal");
-        var btn = document.getElementById("viewFull");
-        var span = document.getElementsByClassName("close")[0];
-        document.getElementById("modalPara").innerHTML = desc;
 
-        btn.addEventListener("click",()=>{
+        button.addEventListener("click",(event)=>{
+            document.getElementById("modalPara").innerHTML = desc;
             modal.style.display = "block";
         })
-          
-        
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-          modal.style.display = "none";
-        }
-        
-        // When the user clicks anywhere outside of the modal, close it
         window.onclick = function(event) {
           if (event.target == modal) {
             modal.style.display = "none";
           }
+          desc.innerHTML = "";
         }
+       
     }
     
 }
+
+   
+
